6fab07f1bd34974444c1a32bd5882931e4893ce9
Deigon Prates <deigon_prates@hotmail.com>
2022-09-18 14:57:33 -0300

    add dashboad simples para exemplificação

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,26 +2,34 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\Simulation;
+use App\Models\SimulationCurrencies;
+use Illuminate\Contracts\Support\Renderable;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class HomeController extends Controller
 {
     /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-    }
-
-    /**
      * Show the application dashboard.
      *
-     * @return \Illuminate\Contracts\Support\Renderable
+     * @return Renderable
      */
-    public function index()
+    public function index(): Renderable
     {
-        return view('home');
+        $currencyOriginList =  Simulation::select(['origin', DB::raw('count(origin) as total')])
+            ->where('user_id', Auth::id())
+            ->orderByDesc('total')
+            ->groupBy('origin')->get();
+        $currencyDestinyList = SimulationCurrencies::select(['currency', DB::raw('count(currency) as total')])
+            ->leftJoin('simulations', 'simulations.id', '=', 'simulation_currencies.simulation_id')
+            ->where('simulations.user_id', Auth::id())
+            ->orderByDesc('total')
+            ->groupBy('currency')->get();
+        $simulationsTotal   =  Simulation::where('user_id', Auth::id())->count();
+        $simulationsValue   =  Simulation::where('user_id', Auth::id())->sum('gross');
+        return view('home', compact(
+            'simulationsTotal', 'simulationsValue', 'currencyOriginList', 'currencyDestinyList'));
     }
+
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,153 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
+    <!-- Main content -->
+    <section class="content">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-3 col-6">
+                    <!-- small box -->
+                    <div class="small-box bg-info">
+                        <div class="inner">
+                            <h3>{{ $currencyOriginList[0]->origin  ?? ' '}}</h3>
+                            <p>Moeda mais utilizada (Origem)</p>
                         </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
+                        <div class="icon">
+                            <i class="ion fa-solid fa-bag-shopping"></i>
+                        </div>
+                    </div>
+                </div>
+                <!-- ./col -->
+                <div class="col-lg-3 col-6">
+                    <!-- small box -->
+                    <div class="small-box bg-success">
+                        <div class="inner">
+                            <h3>{{ $currencyDestinyList[0]->currency  ?? ' '}}</h3>
+                            <p>Moeda mais utilizada (Destino)</p>
+                        </div>
+                        <div class="icon">
+                            <i class="ion fa-solid fa-signal"></i>
+                        </div>
+                    </div>
+                </div>
+                <!-- ./col -->
+                <div class="col-lg-3 col-6">
+                    <!-- small box -->
+                    <div class="small-box bg-warning">
+                        <div class="inner">
+                            <h3 class="text-light">{{ $simulationsTotal }}</h3>
+                            <p class="text-light">Simulações realizadas</p>
+                        </div>
+                        <div class="icon">
+                            <i class="ion fa-solid fa-user-plus"></i>
+                        </div>
+                    </div>
+                </div>
+                <!-- ./col -->
+                <div class="col-lg-3 col-6">
+                    <!-- small box -->
+                    <div class="small-box bg-danger">
+                        <div class="inner">
+                            <h3>{{ number_format($simulationsValue,2,',', '.') }}</h3>
+                            <p>Valor movimentado</p>
+                        </div>
+                        <div class="icon">
+                            <i class="ion fa-solid fa-chart-pie"></i>
+                        </div>
+                    </div>
+                </div>
+                <!-- ./col -->
+            </div>
+            <div class="row">
+                <div class="col-md-6">
+                    <!-- DONUT CHART -->
+                    <div class="card">
+                        <div class="card-header">
+                            <h3 class="card-title">Moedas de origem mais negociadas</h3>
+                            <div class="card-tools">
+                                <button type="button" class="btn btn-tool" data-card-widget="collapse">
+                                    <i class="fas fa-minus"></i>
+                                </button>
+                                <button type="button" class="btn btn-tool" data-card-widget="remove">
+                                    <i class="fas fa-times"></i>
+                                </button>
+                            </div>
+                        </div>
+                        <div class="card-body">
+                            <div id="pieChartOrigin" ></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <!-- DONUT CHART -->
+                    <div class="card">
+                        <div class="card-header">
+                            <h3 class="card-title">Moedas de destino mais negociadas</h3>
+                            <div class="card-tools">
+                                <button type="button" class="btn btn-tool" data-card-widget="collapse">
+                                    <i class="fas fa-minus"></i>
+                                </button>
+                                <button type="button" class="btn btn-tool" data-card-widget="remove">
+                                    <i class="fas fa-times"></i>
+                                </button>
+                            </div>
+                        </div>
+                        <div class="card-body">
+                            <div id="pieChartDestiny" ></div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-</div>
+    </section>
+@endsection
+@section('script')
+    <script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
+    <script>
+        $(function () {
+
+            const chartOrigin = new ApexCharts(document.querySelector("#pieChartOrigin"), {
+                series: [{{  implode(',', $currencyOriginList->pluck('total')->toArray()) }}],
+                chart: {
+                    width: 380,
+                    type: 'pie',
+                },
+                labels: [{!! '"'.implode('","', $currencyOriginList->pluck('origin')->toArray()).'"' !!}],
+                responsive: [{
+                    breakpoint: 480,
+                    options: {
+                        chart: {
+                            width: 200
+                        },
+                        legend: {
+                            position: 'bottom'
+                        }
+                    }
+                }]
+            });
+            chartOrigin.render();
+
+            const chartDestiny = new ApexCharts(document.querySelector("#pieChartDestiny"), {
+                series: [{{  implode(',', $currencyDestinyList->pluck('total')->toArray()) }}],
+                chart: {
+                    width: 380,
+                    type: 'pie',
+                },
+                labels: [{!! '"'.implode('","', $currencyDestinyList->pluck('currency')->toArray()).'"' !!}],
+                responsive: [{
+                    breakpoint: 480,
+                    options: {
+                        chart: {
+                            width: 200
+                        },
+                        legend: {
+                            position: 'bottom'
+                        }
+                    }
+                }]
+            });
+            chartDestiny.render();
+
+        })
+    </script>
 @endsection
